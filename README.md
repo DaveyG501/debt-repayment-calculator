203fd4ef3371b3bdf79bb900c3738e0f292bd478
DaveyG501 <dvdgeisert@gmail.com>
2019-08-26 20:32:25 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## This Site Will Allow You To Plan Your Debt Repayment Timeline
+
+Stay Tuned!
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Howdy Ya'll!</h1>
+    </div>
+  );
+}
+
+export default App;
